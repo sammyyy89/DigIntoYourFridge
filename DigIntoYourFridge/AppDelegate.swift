c37c6277726828a1bb8483da448d39ce5a110a41
Saemi An <saemi0809@gmail.com>
2022-11-05 11:42:12 -0400

    Added new fonts

--- DigIntoYourFridge/AppDelegate.swift
+++ DigIntoYourFridge/AppDelegate.swift
@@ -10,10 +10,14 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        // print font names
+        UIFont.familyNames.forEach({ name in
+                    for font_name in UIFont.fontNames(forFamilyName: name) {
+                        print("\n\(font_name)")
+                    }
+                })
         return true
     }
 
