6251ed41ac7282035ef92e435417acadfc3ca976
Saemi An <saemi0809@gmail.com>
2022-11-05 11:36:33 -0400

    Initial Commit

---
+++ DigIntoYourFridge/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DigIntoYourFridge
+//
+//  Created by Saemi An on 11/5/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
